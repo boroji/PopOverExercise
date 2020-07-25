fcc032a5af8fc9788062c589516faaead5eadbe5
Hossein <hosseinboroji@gmail.com>
2020-07-25 18:36:35 -0400

    added keyboard accessibility and finalized comments

--- js/index.js
+++ js/index.js
@@ -1,34 +1,44 @@
-//  () - Custom Checkbox Toggle
+/* 
+************************************
+Table of Content
+(1) - Custom Checkbox Toggle
+(2) - Checkbox Hide
+(3) - Checkbox Checked
+(4) - Pop Over after 3 seconds
+(5) - Pop Over Close function
+(6) - Click event and keyboard accessibility
+(7) - Simple Parallex
+************************************
+*/
+
+//  (1) - Custom Checkbox Toggle
 const checkbox = document.querySelector('.custom-checkbox');
 const checked = document.querySelector('.custom-checked');
 
-//  () - Checkbox Hide
+//  (2) - Checkbox Hide
 checkbox.addEventListener('click', () => {
     checkbox.classList.toggle('hide')
     checked.classList.toggle('hide')
 })
 
-//  () - Checkbox Checked
+//  (3) - Checkbox Checked
 checked.addEventListener('click', () => {
     checkbox.classList.toggle('hide')
     checked.classList.toggle('hide')
 })
 
-//  () - Pop Over 3 seconds
+//  (4) - Pop Over after 3 seconds
 const popOver = document.querySelector('.pop-over');
 const wrapper = document.querySelector('.wrapper');
 const overlay = document.querySelector('.overlay');
-
 
 setTimeout(() => {
     popOver.classList.add('visible');
     overlay.classList.add('visible');
 }, 3000);
 
-//  () - Close button trigger
-const btn = document.querySelector('.close-button')
-
-btn.addEventListener('click', () => {
+//  (5) - Pop Over Close function
+function modalClose() {
     popOver.classList.add('close')
     overlay.classList.add('close')
     setTimeout(() => {
@@ -36,9 +46,29 @@ btn.addEventListener('click', () => {
         wrapper.style.display = 'none';
         overlay.style.display = 'none';
     }, 1200);
+}
+
+//  (6) - Click event and keyboard accessibility
+const btn = document.querySelector('.close-button')
+btn.addEventListener('click', () => {
+    modalClose();
 })
 
-//  () - Simple Parallex
+// Closing X button using enter keyboard
+btn.addEventListener('keypress', (e) => {
+    if (e.keyCode === 13) {
+        modalClose();
+    }
+})
+
+// Closing Modal using 'esc' button
+popOver.addEventListener('keydown', (e) => {
+    if (e.keyCode === 27) {
+        modalClose();
+    }
+})
+
+//  (7) - Simple Parallex
 const shavingCream = document.querySelector('.shaving-cream');
 const knife = document.querySelector('.knife');
 
